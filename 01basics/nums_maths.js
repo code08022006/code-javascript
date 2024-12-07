6a5829bff9316320e4902d01e13f7938bf5645e4
code08022006 <08adityajadhav@gmail.com>
2024-12-07 13:16:38 +0000

    study math very imp

---
+++ 01basics/nums_maths.js
@@ -0,0 +1,53 @@
+const score=300;
+// console.log(score)
+const balance=new Number(400); // use this syntax to define a number
+// console.log(balance);
+// console.log(balance.toString());
+// console.log( balance);
+
+const num=1000000;
+// console.log(num.toLocaleString('en-In'));
+const num1=1234.5656;
+// console.log(num1.toFixed(2));
+// // ++++++++++++++maths++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4)); //absolute function converts negative value to positive and positive remains same
+// const d=Math.PI;
+
+// console.log(d*4*4);
+// console.log(Math.round(32.7));
+// console.log(Math.ceil(4.2))// ceil means that if value of 4 is little bit be larger than it take higher value 5
+// console.log(Math.floor(4.5)); // floor means that if value of 4 is little bit larger ten it takes lower value 4
+// console.log(Math.sqrt(4));
+// console.log(Math.sin(d/2));
+// console.log(Math.sinh(0));
+// console.log(Math.min(2,5,6,1));
+// console.log(Math.max(2,3,452,3));
+
+// console.log(Math.random()); // the value of math.random is always between 0 and 1
+// console.log(Math.floor(Math.random()*10));
+
+const min=10;
+const max=20;
+const f = Math.floor((Math.random()*(max-min+1))+min);// learn this formula very i.m.p
+console.log(f);
+// math.random generates number between 1 and 2
+// here we define max and min
+// then max-min defines the range of numbers and+1 is for to generate numbers greater than zero
+//plus min to get that at least 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
