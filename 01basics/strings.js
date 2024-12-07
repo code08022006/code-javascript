e53d9e63f5272f327f51a600bfe2cf17a3da7e59
code08022006 <08adityajadhav@gmail.com>
2024-12-07 12:16:40 +0000

    study strings

--- 01basics/strings.js
+++ 01basics/strings.js
@@ -1,5 +1,37 @@
-const name="aditya";
-const repoCount=50;
-console.log(`Hello my name is ${name} my repo count is ${repoCount}`); // variable insertion in string
-const gameNmae= new String("adityahc"); // please type this code in console and see magic
-// after writing this code in console it gives index of each letter and length of string
+// const kname="aditya";
+// const repoCount=50;
+// console.log(`Hello my name is ${kname} my repo count is ${repoCount}`); // variable insertion in string
+// const gameNmae= new String("adityahc"); // please type this code in console and see magic
+// // after writing this code in console it gives index of each letter and length of string
+// console.log(kname.toUpperCase());
+// console.log(kname.charAt(2));
+// console.log(kname. __proto__);
+// // console.log(kname.fontcolor(blue))
+// const newString=kname.substring(0,3);
+// console.log(newString);
+// const anotherString=kname.slice(-6,3);
+// console.log(anotherString);
+
+const newOne="  hitesh  ";
+console.log(newOne.trim());
+const url="https://hitesh/hitest%20.com";
+console.log(url.replace('%20','-'));
+const Name="aditya rajendra jadhav";
+console.log(Name.split(' ')); // this will convert string to array on the basics of spaces
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
