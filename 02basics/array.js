cf2a1022c9bb55c10836a40ae335812d6555b7c5
code08022006 <08adityajadhav@gmail.com>
2024-12-08 05:47:20 +0000

    study array

---
+++ 02basics/array.js
@@ -0,0 +1,72 @@
+const myArr=[1,2,3,4,5];
+const newArr=new Array(1,2,3,4,5);
+const heroes=["ironman","shaktiman","batman"];
+console.log(myArr[0]);
+
+
+// array methods
+// myArr.push(9);
+
+// console.log(myArr);
+// myArr.pop(); // remove last value
+// console.log(myArr)
+//  myArr.unshift(9); // add 9 at the begining of array
+//  console.log(myArr);
+//  myArr.shift(); // removes first element
+//  console.log(myArr);
+//  console.log(myArr.includes(9));
+//  console.log(myArr.indexOf(9));
+//  const adArr=myArr.join(","); // THIS WILL CONVERT ARRAY TO STRING  
+//  console.log(adArr);
+  //++++++++++++++++++ slice and splice++++++++++++++++++
+//   const adrArr=[0,1,2,3,4,6];
+//   console.log("A",adrArr);
+//   console.log(adrArr.slice(1,3));
+//   console.log("b",adrArr);
+//   console.log(adrArr.splice(1,3)); // splice will remove the values index as 1 and 2 and 3 from given array
+//   console.log("c",adrArr);
+  
+const myn1=new Array(1,2,3,4,5,6);
+const myn2=[4,5,6,7];
+// myn1.push(myn2);
+// console.log(myn1[6][0]);
+// const a=myn1.concat(myn2);
+// console.log(a);
+
+//smple metnod to concat is spread.take a glass and drop it will spread
+// const b=[...myn1,...myn2];
+// console.log(b);
+
+// const another_array=[1,2,3,[3,45,6],7,5,[2,3,4,[4,6,7]]];
+// const real_another_array=another_array.flat(Infinity);
+// console.log(real_another_array);
+
+console.log(Array.isArray("hitesh")); // the element providede in ()this is array or not is checked
+console.log(Array.from("hitesh")); // the element providee in ( )this is converted to array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+
+ 
+ 
+ 
+
