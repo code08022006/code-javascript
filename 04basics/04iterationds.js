bdbaf986f74446aea8965d94cf89c7f25935603b
Adi022006 <08adityajadhav@gmail.com>
2024-12-22 13:32:00 +0000

    iterations

---
+++ 04basics/04iterationds.js
@@ -0,0 +1,35 @@
+// for
+for (let i = 0; i <=10;i++)
+{
+    const element = i;
+    // console.log(element);
+    
+    
+}
+// for (let i = 1; i <=10; i++) {
+//     if(i===5){
+//         console.log("5 is detected")
+//         break;
+//     }
+//     console.log(`value is: ${i}`);
+    
+    
+// }
+for (let i = 1; i <=10; i++) {
+    if(i===5){
+        console.log("5 is detected")
+        continue;
+    }
+    console.log(`value is: ${i}`);
+    
+    
+}
+
+
+
+
+
+
+
+
+
